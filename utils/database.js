3a6fb5d05cae5d0cf453f10d346968ab475c2230
Yashwanth Reddy <yashwanthreddy072@gmail.com>
2024-03-15 11:16:09 +0530

    final commit

---
+++ utils/database.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+let isConnected = false
+
+export const connectToDB = async()=>{
+    mongoose.set('strictQuery',true)
+
+    if(isConnected){
+        console.log('MongoDb is already Connected')
+
+    }
+    else{
+        try {
+            await mongoose.connect(process.env.MONGODB_URI,
+                {dbName:"share_prompt",
+                useNewUrlParser:true,
+                useUnifiedTopology:true,
+            })
+
+            isConnected = true;
+            console.log("MongoDb Connected")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
