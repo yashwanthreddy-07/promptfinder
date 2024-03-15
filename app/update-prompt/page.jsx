de099b5e7071b7ff348117fa87d795797b5bff0f
Yashwanth Reddy <yashwanthreddy072@gmail.com>
2024-03-15 12:07:14 +0530

    fixed suspense

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Suspense } from "react";
+
 import { useRouter,useSearchParams } from "next/navigation";
 import From from "@components/Form";
 const EditPrompt = () => {
@@ -51,7 +51,7 @@ const EditPrompt = () => {
   };
 
   return (
-    <Suspense>
+    
     <From
       type="Edit "
       post={post}
@@ -59,7 +59,7 @@ const EditPrompt = () => {
       submitting={submitting}
       handleSubmit={updateePrompt}
       ></From>
-  </Suspense>
+  
   );
 };
 
